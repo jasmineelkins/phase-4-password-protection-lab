c98c996adc612da4bb96e5360e1078baaed7804e
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-05-06 10:41:19 -0400

    commit tree 00fdd3fd50cc6abe1f2b557aaf35a80f27819fe4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  # before_action :authorized
+
+  # def authorized
+  #   unless session.include? :user_id
+  #     return render json: { error: 'Not authorized' }, status: :unauthorized
+  #   end
+  # end
 end
